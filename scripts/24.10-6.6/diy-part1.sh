7946de230c69c20d8f2845d34cbe903dfafce6fe
zikexiaozi <97644359@qq.com>
2026-03-30 23:05:23 +0800

    Update diy-part1.sh

--- scripts/24.10-6.6/diy-part1.sh
+++ scripts/24.10-6.6/diy-part1.sh
@@ -9,4 +9,10 @@
 # 文件名: diy-part1.sh
 # 描述: OpenWrt DIY 脚本第一部分 (更新 feeds 之前)
 
-
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone https://github.com/sbwml/luci-app-openlist package/openlist
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+git clone https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
+git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
